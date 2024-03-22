a406305726bd34057b7bfbb5fe28aa7dfbbbb612
almaz <almazsyd5@gmail.com>
2024-03-22 20:58:41 +0600

    Initial Commit

---
+++ TablevView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TablevView
+//
+//  Created by Mac on 22/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
